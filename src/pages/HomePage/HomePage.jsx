4b8b567722cd72cad6f48df8fbcf38d25cdae3e3
Eli Sacks <bastlifa@gmail.com>
2019-07-27 22:05:07 -0400

    styled menu items and directory

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.scss'
+import Directory from '../../components/Directory/Directory'
+const HomePage = _ =>
+{
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
